8e7ee06b9432b108a1b18e880475012e2329ed4e
Anish Das <135655078+ragingtitan@users.noreply.github.com>
2024-03-12 20:07:37 +0000

    changes made

--- Project/index.js
+++ Project/index.js
@@ -10,10 +10,6 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.get('/', (req, res) => {
   res.sendFile(path.join(__dirname+ '/public/index.html'));
 });
-
-app.get('/dashboard', (req, res) => {
-    res.sendFile(path.join(__dirname+ '/public/index.html'));
-});
   
 app.listen(port, () => {
     console.log(`Server is running on http://localhost:${port}/`);
